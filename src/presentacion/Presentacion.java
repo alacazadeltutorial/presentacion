dd75d8b1ca012c73f4919095f786d0e257e93e68
alacazadeltutorial <alacazadeltutorial@gmail.com>
2018-04-11 10:12:41 +0200

    Creado proxeto Presentación

---
+++ src/presentacion/Presentacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentacion;
+
+/**
+ *
+ * @author uadmin
+ */
+public class Presentacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Preséntate, indicando o teu nome completo. Se hai mais nomes, colócao na seguinte liña");
+        System.out.println("Alonso Beltrán Grande");
+        
+    }
+    
+}
